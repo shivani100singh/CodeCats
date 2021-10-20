f45929b2597b947d9d74bdd01754a82f49e165ff
Shivani Singh <shivani9115@gmail.com>
2021-10-20 18:46:19 +0530

    maximum among two numbers

---
+++ week1/Q2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int number_1,number_2,max;
+    printf("Enter two numbers: ");
+    scanf("%d" "%d",&number_1,&number_2);
+    max=(number_1>number_2?number_1:number_2);
+    printf("%d",max);
+
+    return 0;
+}
