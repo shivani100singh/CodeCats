fcb101f7adf8f69aabc9cd329fa9d972493e7b43
Shivani Singh <shivani9115@gmail.com>
2021-10-27 20:44:22 +0530

    updated number of zeros in n!

--- week2/Q7/no_of_zeros_in_n_factorial.c
+++ week2/Q7/no_of_zeros_in_n_factorial.c
@@ -1,16 +1,34 @@
 #include<stdio.h>
+#include<stdlib.h>
 void main()
 {
-    long int number,no_of_zeros,sum=0,n,pro,i;
-    printf("Enter the number: ");
-    scanf("%d",&number);
+    long int number,no_of_zeros,sum=0,n,num,i,pro;
+    int *store;int *result;
+    printf("Enter number of numbers to follow: ");
+    scanf("%d",&n);
+    num=n;
+    store=(int*)malloc(n*sizeof(int));
+    result=(int*)malloc(n*sizeof(int));
+    for(i=0;i<n;i++)
+       scanf("%d",&store[i]);
+    
+    i=0;
+    while (n--)
+    {
+    sum=0;
+    number=store[i];
     pro=1,no_of_zeros=number;
-    while(no_of_zeros>0)
-    {   
-        pro=5*pro;
-        no_of_zeros=number/pro;
-        sum=sum+no_of_zeros;
+        while(no_of_zeros>0)
+            {   
+               pro=5*pro;
+               no_of_zeros=number/pro;
+               sum=sum+no_of_zeros;
+            }
+    result[i]=sum;
+    i++;
     }
-    printf("Number of zeros in %d is: %d",number,sum);
+    
+    for(i=0;i<num;i++)
+    printf("Number of zeros in %d is: %d\n",store[i],result[i]);
 
 }
